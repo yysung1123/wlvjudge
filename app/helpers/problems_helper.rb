658fe46f03e93ff021a861c89bfbfee23219e75c
tsaodingtw <tsaodingtw@gmail.com>
2015-12-10 02:36:51 +0800

    add zj parser

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -7,4 +7,23 @@ module ProblemsHelper
     sample_input = doc.css('pre').first.text
     sample_output = doc.css('pre').last.text
   end
+
+  def zerojudge_parser(problemid)
+  	
+  	doc = Nokogiri::HTML(open('http://zerojudge.tw/ShowProblem?problemid=' + problemid))
+	
+	a = {}
+	
+	a['title'] = doc.css('span#problem_title')[0].content
+	a['content'] = doc.css('div#problem_content')[0].content
+	a['input'] = doc.css('div#problem_theinput')[0].content
+	a['output'] = doc.css('#problem_theoutput')[0].content
+	a['sample_input'] = doc.css('div.problembox pre')[0].content
+	a['sample_output'] = doc.css('div.problembox pre')[1].content
+	a['hint'] = doc.css('div#problem_hint')[0].content
+	
+	return a
+
+  end
+
 end
