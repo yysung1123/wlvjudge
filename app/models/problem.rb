058ad800d97aa8287f91a86dd369a76ff80ee4e6
deror1869107 <sungyuanyao@gmail.com>
2015-12-12 14:44:40 +0800

    Uniqueness for Problems

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -3,6 +3,6 @@ class Problem < ActiveRecord::Base
   has_many :contests, :through => :contest_problem_sets
   has_many :submissions
   belongs_to :judge
-
+  validates :judge, uniqueness: { scope: :source }
   #validates , presence: true
 end
